5153f713eaa7f11fab47c9fbad669c12f56656d8
Alexlogvin2019 <alexlogvin@yahoo.com>
2020-03-05 14:27:53 +0200

    Updates

---
+++ precache-manifest.d0ee09def6454da0899dc61239ea47e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d68a4d49904718966a5bdd3f1c4f326",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "3de3e00142f34095d68c",
+    "url": "/goit-react-hw-01-components/static/css/main.b8b5dba2.chunk.css"
+  },
+  {
+    "revision": "de3ffdbd2d40a79d1483",
+    "url": "/goit-react-hw-01-components/static/js/2.7896a2aa.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/goit-react-hw-01-components/static/js/2.7896a2aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3de3e00142f34095d68c",
+    "url": "/goit-react-hw-01-components/static/js/main.404615bc.chunk.js"
+  },
+  {
+    "revision": "d1223a56ce2b31e92e84",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.687648de.js"
+  }
+]);
